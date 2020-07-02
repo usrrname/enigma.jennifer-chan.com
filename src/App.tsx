42191fc34c042fa5f41f01ab5819ee2cc29b0a3e
Jen Chan <6406037+usrrname@users.noreply.github.com>
2020-07-01 21:05:12 -0400

    store videos in an enum

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,42 @@ import { Wallpaper } from './components/wallpaper';
 import { Container } from 'react-bootstrap';
 import { useElapsedTime } from 'use-elapsed-time';
 import useSound from 'use-sound';
+import { YouTubeProps } from 'react-youtube';
 
 const soundtrack = require('./assets/sadeness.mp3');
 
-export const App = () => {
+export enum videoIdList {
+  WHALE = "TPChnsfBFuw",
+  DEAD_WHALE = "IZRruR6X220",
+  JACOB_REES = "XJ8i896xM",
+  NOODLE = "5mTiFRNMRO0",
+  SPAM = "uamdraznYsU"
+}
+
+type Props = YouTubeProps;
+
+export const App = (props: Props) => {
+
+  const [videoId, setVideoId] = useState(videoIdList.WHALE);
 
   let [isChecked, setIsChecked] = useState(false);
 
   const { elapsedTime } = useElapsedTime(isChecked);
-  const soundCheckBox = document.querySelector('.sound-toggle');
+  const soundCheckBox = document.querySelector('.toggle-sound');
 
   useEffect(() => {
     soundCheckBox?.addEventListener("click", () => {
-      if (soundCheckBox.getAttribute('active')) {
+      if (document.querySelector('.active')) {
         setIsChecked(prevIsPlaying => !prevIsPlaying);
       }
     });
   })
 
   let [play, { pause, isPlaying }] = useSound(soundtrack);
+
+  const onStateChange = (event: any) => {
+    setVideoId(videoId);
+  }
 
   return (
     <div className="App">
@@ -36,14 +53,16 @@ export const App = () => {
           pause={pause}
         />
         <span className="elapsed-time">{elapsedTime}</span>
+
         <Wallpaper
+          videoId={videoId}
           className="videoWrapper"
           isChecked={isChecked}
           isPlaying={isPlaying}
+          start={props.opts?.playerVars?.start}
+          end={props.opts?.playerVars?.end}
+          onStateChange={onStateChange}
         ></Wallpaper>
-
-        {/* TODO: Figure out how to jump between one and the next video */}
-        {/* <Wallpaper className="videoWrapper" videoId="IZRruR6X220"></Wallpaper> */}
       </Container>
     </div >
   );
--- src/components/wallpaper.tsx
+++ src/components/wallpaper.tsx
@@ -1,21 +1,17 @@
 import React, { useState } from 'react';
 import YouTube, { YouTubeProps, Options, PlayerVars } from 'react-youtube';
 import { ResponsiveEmbed } from 'react-bootstrap';
+import { videoIdList } from '../App';
 
 type Props = YouTubeProps & {
   isPlaying?: boolean;
   isChecked?: boolean;
-}
-
-enum videoIdList {
-  FIRST_VIDEO = "TPChnsfBFuw",
-  SECOND_VIDEO = "IZRruR6X220",
-  THIRD_VIDEO = "XJ8i896xM"
+  start?: number;
+  end?: number;
 }
 
 export const Wallpaper = (props: Props) => {
-  let { isChecked, isPlaying } = props;
-  const [videoId, setVideoId] = useState(videoIdList.FIRST_VIDEO);
+  let { isChecked, isPlaying, start, end } = props;
   const [player, setPlayer] = useState(null);
 
   const onReady = (event: any) => {
@@ -23,9 +19,7 @@ export const Wallpaper = (props: Props) => {
   }
 
   const onPlay = (event: any) => {
-    if (isChecked && isPlaying) {
-      event.target.playVideo();
-    }
+    event.target.playVideo();
   }
 
   const onPause = (event: any) => {
@@ -34,35 +28,34 @@ export const Wallpaper = (props: Props) => {
     }
   }
 
-  const onStateChange = (event: any) => {
-    setVideoId(videoId === videoIdList.FIRST_VIDEO ? videoIdList.FIRST_VIDEO : videoIdList.SECOND_VIDEO);
-  }
-
   const opts = {
     playerVars: {
       controls: 0,
       color: 'red',
-      enablejsapi: 1,
       playsinline: 1,
       mute: 1,
+      fs: 0,
+      disablekb: 0,
       modestbranding: 1,
       showinfo: 0,
       // https://developers.google.com/youtube/player_parameters
-      autoplay: 0
+      autoplay: 1,
+      start: start,
+      end: end
     } as PlayerVars,
   } as Options;
 
   return (
     <ResponsiveEmbed aspectRatio='16by9'>
       <YouTube
-        videoId={videoId}
+        videoId={props.videoId}
         className={props.className}
         containerClassName={props.containerClassName}
         opts={opts}
         onReady={onReady}
         onPlay={onPlay}
         onPause={onPause}
-        onStateChange={onStateChange}
+        onStateChange={props.onStateChange}
       />
     </ResponsiveEmbed>
   );

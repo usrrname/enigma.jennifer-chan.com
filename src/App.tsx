d62a37ec3866672dc57eb88d0550486df53354e1
Jen Chan <6406037+usrrname@users.noreply.github.com>
2020-07-03 19:33:16 -0400

    Add rest of videos to the enum

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,38 @@ import { YouTubeProps } from 'react-youtube';
 const soundtrack = require('./assets/sadeness.mp3');
 
 export enum videoIdList {
-  WHALE = "TPChnsfBFuw",
+  PLANE_WASHES = "SNDGCiY0z5I",
+  BABY_WHALE = "TPChnsfBFuw",
   DEAD_WHALE = "IZRruR6X220",
   JACOB_REES = "XJ8i896xM",
   NOODLE = "5mTiFRNMRO0",
-  SPAM = "uamdraznYsU"
+  SPAM = "uamdraznYsU",
+  MERKEL_SHAKING = "ilEolWazM6o",
+  MACE = "aKmLxoMScb4",
+  BUBBLING_MUD = "se_Q_KRGTNg",
+  DRINKING_WATER = "BhAX0gi4CbM",
+  DOG_DRINKING = "_OT4CNxHEGM",
+  NEVER_SCOOP = "2rCVARgnyjg",
+  POLAR_VORTEX = "1xTG5pHSjYQ",
+  MILK_TEA = "L9TnPiCVaLI",
+  BUCKET = "WIo9ROTi7a4",
+  TRASH_MP = "A8q-Zx8gIbg",
+  FLUSH_NOODLES = "zQVXFpDb5zM",
+  SNAKE_EATING_SELF = "jIl2DSXUffw",
+  ROPE_WALKING = "XtPic_hf-Jo"
 }
 
 type Props = YouTubeProps;
 
 export const App = (props: Props) => {
 
-  const [videoId, setVideoId] = useState(videoIdList.WHALE);
+  const [videoId, setVideoId] = useState(videoIdList.PLANE_WASHES);
 
   let [isChecked, setIsChecked] = useState(false);
 
   const { elapsedTime } = useElapsedTime(isChecked);
 
   let [play, { pause, isPlaying }] = useSound(soundtrack);
-
-  let [vplayer, setPlayer] = useState(false);
 
   const onStateChange = (event: any) => {
     setVideoId(videoId);

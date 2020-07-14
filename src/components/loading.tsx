c3d777082a5dbdc03566a9e66b70acf61105f66c
Jen Chan <6406037+usrrname@users.noreply.github.com>
2020-07-14 12:49:53 -0400

    Add image

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Container, Row, Spinner } from 'react-bootstrap';
 
+
 export const Loading = () => {
+  const guillotine = require('../assets/guillotine.jpg');
+
   return (
     <Container>
       <Row>
-        <Spinner animation="border" variant="danger" />
-        <Spinner animation="border" variant="warning" />
-        <Spinner animation="border" variant="info" />
+        <Spinner animation="grow" variant="danger" />
+        <img src={guillotine} alt="French Revolution Execution with Guillotine" />
       </Row>
     </Container>)
 }

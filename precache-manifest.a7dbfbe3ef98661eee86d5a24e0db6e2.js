eb429edd94bb711136b249794a76c9f7af7d7a5c
Jen Chan <6406037+usrrname@users.noreply.github.com>
2020-07-14 16:03:34 -0400

    Updates

---
+++ precache-manifest.a7dbfbe3ef98661eee86d5a24e0db6e2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4890badf9ddb32fc9b478dd8546e3f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eff1b481296c09a3b19",
+    "url": "/static/css/3.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a0acd8dbe397fda35f9f",
+    "url": "/static/css/main.54fe3bb0.chunk.css"
+  },
+  {
+    "revision": "7723a1760a728e4c13d9",
+    "url": "/static/js/2.7b23ac2c.chunk.js"
+  },
+  {
+    "revision": "a73ae50cc0b9f13c21dd696a093a7ee8",
+    "url": "/static/js/2.7b23ac2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eff1b481296c09a3b19",
+    "url": "/static/js/3.ace65ba7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/3.ace65ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5539ca46fe82b5f029c9",
+    "url": "/static/js/4.e0c3116a.chunk.js"
+  },
+  {
+    "revision": "c3fcfc539d2c603771c3",
+    "url": "/static/js/5.2e90e5d9.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/static/js/5.2e90e5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eea94e05ffad7ecbd96",
+    "url": "/static/js/6.f1a3724d.chunk.js"
+  },
+  {
+    "revision": "a0acd8dbe397fda35f9f",
+    "url": "/static/js/main.92a22585.chunk.js"
+  },
+  {
+    "revision": "c3fa22eae9952cd25d8f",
+    "url": "/static/js/runtime-main.fe675103.js"
+  },
+  {
+    "revision": "5dcc518e86775d0ff35b6b63f5549cc5",
+    "url": "/static/media/sadeness.5dcc518e.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/enigma.jennifer-chan.com/precache-manifest.b26e87ce5ffdaf3d613ab08e3ec2c89c.js"
+  "/precache-manifest.a7dbfbe3ef98661eee86d5a24e0db6e2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/enigma.jennifer-chan.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
